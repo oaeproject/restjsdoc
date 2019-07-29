711fc6249634175f6fcedc9f45081d7a10b943af
Miguel Laginha <me@miguellaginha.com>
2019-07-29 10:44:50 +0100

    chore: XOd some files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,16 @@
+/* eslint-disable unicorn/filename-case */
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-release');
 
-    grunt.initConfig({
-        'release': {
-            'options': {
-                'github': {
-                    'repo': 'oaeproject/restjsdoc',
-                    'usernameVar': 'GITHUB_USERNAME',
-                    'passwordVar': 'GITHUB_PASSWORD'
-                }
-            }
+  grunt.initConfig({
+    release: {
+      options: {
+        github: {
+          repo: 'oaeproject/restjsdoc',
+          usernameVar: 'GITHUB_USERNAME',
+          passwordVar: 'GITHUB_PASSWORD'
         }
-    });
+      }
+    }
+  });
 };
